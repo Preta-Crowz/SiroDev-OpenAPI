326242fca8c628a81a42bcc8a06abede856c4933
Preta-Crowz <preta@siro.dev>
2021-08-31 07:37:15 +0900

    끄투 관련 API 제공 중단 및 의미없는 로깅 제거

--- api.py
+++ api.py
@@ -7,256 +7,33 @@ import zerologger as zlog
 logger = zlog.Logger(config["RAVEN"]["KEY"],config["RAVEN"]["SECRET"],config["RAVEN"]["PROJECT"],config["APPNAME"],config["LOG"])
 
 from flask import Flask,render_template,request,send_file
-logger.debug("flask imported")
 import Levenshtein as leven
-logger.debug("Levenshtein imported")
 try:
     from winmagic import magic
 except:
     import magic
-logger.debug("magic imported")
 import base64
-logger.debug("base64 imported")
 import time
-logger.debug("time imported")
 import re
-logger.debug("re imported")
 import requests
-logger.debug("requests imported")
 import socket
-logger.debug("socket imported")
 import urllib3
-logger.debug("urllib3 imported")
 import execjs
-logger.debug("execjs imported")
 logger.info(f"execjs started with {execjs.get().name}")
 import ast
-logger.debug("ast imported")
 
 
 import squarecrypt as sqc
-logger.debug("squarecrypt imported")
 
 logger.info("Setting up application")
 app = Flask(config["APPNAME"])
 
 import logging
 logging.getLogger('werkzeug').setLevel(-100)
-logger.debug("Flask logging disabled")
-
-url = [
-    # {'code':'kkr','name':'끄투코리아','url':'https://kkutu.co.kr/'},
-    {'code':'com','name':'끄투닷컴','url':'http://kkutu.club/'},
-    {'code':'han','name':'끄투한국','url':'https://kkutu.cc/'},
-    {'code':'pkt','name':'프로젝트 끄투','url':'https://prjkt.online/'},
-    {'code':'bfk','name':'BF끄투','url':'https://bfk.playts.net/'},
-    {'code':'mdk','name':'모두끄투','url':'https://modukkutu.kro.kr/'},
-    {'code':'nbk','name':'나비끄투','url':'https://nabitu.ze.am/'},
-    {'code':'k25','name':'끄투25시','url':'http://kt24.playts.net/'}
-]
 
 @app.route('/', methods=['GET'])
 def help():
     return response(render_template("index.html"))
-
-
-
-@app.route('/urls', methods=['GET', 'POST'])
-def urls():
-    global url
-    data = {}
-    data['api'] = {'date':'190806'}
-    data["url"] = url
-    return response(data)
-
-
-
-@app.route('/word', methods=['POST'])
-def get_word():
-    rdat = request.form.to_dict()
-    if rdat == {}: rdat = request.json
-    word = rdat["word"]
-    lms = rdat.get('listing', '0')
-    start = time.time()
-    global url
-    data = {}
-    
-    if lms != '0' and lms != '1':
-        return response('<h3>대신 FFF를 드립니다.</h3><iframe width="560" height="315" src="https://www.youtube.com/embed/Vo5WvNfOuAM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
-
-    if re.match("[a-zA-Z0-9 ]*",word)[0] == word:
-        lang = "en"
-    elif re.match("[ㄱ-ㅣ가-힣0-9 ]*",word)[0] == word:
-        lang = "ko"
-    else:
-        lang = "nul"
-    data['req'] = {'word':word,'lang':lang,'server':'all'}
-    data['api'] = {'version':'1.0','date':'190806'}
-    if lang == 'nul':
-        data['error'] = {"code":"unk_lang","text":"언어 인식에 실패하였습니다. 단어를 맞게 입력했는지 확인하시고, 해당 현상이 지속되면 관리자에게 문의하세요."}
-        return response(data)
-
-    for sv in url:
-        # print('word : {}/{}'.format(sv["code"],word))
-        cfail = False
-        try:
-            r = requests.get(sv["url"]+'dict/{}?lang={}'.format(word,lang),timeout=1)
-        except requests.exceptions.Timeout:
-            data[sv["code"]] = {'responce':'nul','error':{'code':'timedout','text':'서버 연결에 실패했습니다. 서버 응답이 늦거나, 서버가 오프라인입니다.'}}
-            cfail = True
-        except Exception as e:
-            data[sv["code"]] = {'responce':'nul','error':{'code':'unk_conn','text':'서버 연결에 실패했습니다. 원인 추적에 실패하였습니다.','exception':{'name':str(e),'traceback':str(e.with_traceback(None))}}}
-            cfail = True
-        if cfail: continue
-
-        if r.status_code == 200:
-            svd = json.loads(r.text)
-        else:
-            svd = {'responce':r.status_code}
-        if 'word' in svd: svd.pop('word')
-        if 'error' in svd:
-            svd['responce'] = svd.pop('error')
-        else:
-            svd['responce'] = 200
-        if svd['responce'] == 404:
-            svd['error'] = {'code':'notfound','text':'없는 단어입니다.'}
-            data[sv["code"]] = svd
-            continue
-        elif svd['responce'] == 400:
-            svd['error'] = {'code':'badreque','text':'권한이 없습니다.'}
-            data[sv["code"]] = svd
-            continue
-
-        if lms == '1':
-            svd['theme'] = svd['theme'].split(",")
-            svd['type'] = svd['type'].split(",")
-            if sv["code"] == 'han':
-                svd['mean'] = [[re.split("\＂\d*\＂",svd['mean'])[1:]]]
-            else:
-                mean = svd['mean']
-                mean = re.split("\＂\d*\＂",mean)[1:]
-                tmp1 = []
-                final = []
-                for m in mean:
-                    tmp1 = (re.split("\［\d*\］",m)[1:])
-                    tmp2 = []
-                    for t in tmp1:
-                        tmp2.append(re.split("\（\d*\）",t)[1:])
-                    final.append(tmp2)
-                svd['mean'] = final
-        data[sv["code"]] = svd
-
-    data['api']['responce_time'] = time.time() - start
-    return response(data)
-
-
-
-@app.route('/status', methods=['GET', 'POST'])
-def get_info():
-    start = time.time()
-    global url
-    data = {}
-    data['api'] = {'version':'1.0','date':'190806'}
-
-    for sv in url:
-        print('info : {}'.format(sv['code']))
-        cfail = False
-        try:
-            r = requests.get(sv['url']+'servers',timeout=1)
-        except requests.exceptions.Timeout:
-            data[sv["code"]] = {'responce':'nul','error':{'code':'timedout','text':'서버 연결에 실패했습니다. 서버 응답이 늦거나, 서버가 오프라인입니다.'}}
-            cfail = True
-        except Exception as e:
-            data[sv["code"]] = {'responce':'nul','error':{'code':'unk_conn','text':'서버 연결에 실패했습니다. 원인 추적에 실패하였습니다.','exception':{'name':str(e),'traceback':str(e.with_traceback(None))}}}
-            cfail = True
-        if cfail: continue
-
-        if r.status_code == 200:
-            svd = json.loads(r.text)
-        else:
-            svd = {'responce':r.status_code}
-        if 'error' in svd:
-            svd['responce'] = svd.pop('error')
-        else:
-            svd['responce'] = 200
-        if svd['responce'] == 404:
-            svd['error'] = {'code':'notfound','text':'없는 페이지입니다.'} # 출력될 일이 없어야한다.
-            data[sv["code"]] = svd
-            continue
-        elif svd['responce'] == 400:
-            svd['error'] = {'code':'badreque','text':'권한이 없습니다.'} # 아마 이 것도 출력 될 일이 없을 것이다.
-            data[sv["code"]] = svd
-            continue
-        svd['all'] = {'current':0,'max':0}
-        for sl in svd['list']:
-            if sl == None:
-                continue
-            svd['all']['current'] += sl
-            svd['all']['max'] += svd['max']
-        data[sv["code"]] = svd
-
-    data['api']['responce_time'] = time.time() - start
-    return response(data)
-
-
-
-@app.route('/starts', methods=['POST'])
-def starts():
-    rdat = request.form.to_dict()
-    if rdat == {}: rdat = request.json
-    query = rdat["query"]
-    data = {}
-    if re.match("[a-zA-Z0-9 ]*",query)[0] == query:
-        lang = "en"
-    elif re.match("[ㄱ-ㅣ가-힣0-9 ]*",query)[0] == query:
-        lang = "ko"
-    else:
-        lang = "nul"
-    data['req'] = {'query':query,'lang':lang}
-    data['api'] = {'version':'1.0','date':'190806'}
-    if lang == 'nul':
-        data['error'] = {"code":"unk_lang","text":"언어 인식에 실패하였습니다. 단어를 맞게 입력했는지 확인하시고, 해당 현상이 지속되면 관리자에게 문의하세요."}
-        return response(data)
-    r = requests.get(f"https://prjkt.online/starts/{query}?lang={lang}",timeout=1)
-    if r.status_code == 200:
-        rdat = json.loads(r.text)
-    code = r.status_code
-    if code == 400:
-        data['error'] = {'code':'badreque','text':'권한이 없습니다.'} # 아마 이 것도 출력 될 일이 없을 것이다.
-        data['list'] = []
-    else:
-        data['list'] = rdat
-    return response(data)
-
-@app.route('/ends', methods=['POST'])
-def ends():
-    rdat = request.form.to_dict()
-    if rdat == {}: rdat = request.json
-    query = rdat["query"]
-    data = {}
-    if re.match("[a-zA-Z0-9 ]*",query)[0] == query:
-        lang = "en"
-    elif re.match("[ㄱ-ㅣ가-힣0-9 ]*",query)[0] == query:
-        lang = "ko"
-    else:
-        lang = "nul"
-    data['req'] = {'query':query,'lang':lang}
-    data['api'] = {'version':'1.0','date':'190806'}
-    if lang == 'nul':
-        data['error'] = {"code":"unk_lang","text":"언어 인식에 실패하였습니다. 단어를 맞게 입력했는지 확인하시고, 해당 현상이 지속되면 관리자에게 문의하세요."}
-        return response(data)
-    r = requests.get(f"https://prjkt.online/ends/{query}?lang={lang}",timeout=1)
-    if r.status_code == 200:
-        rdat = json.loads(r.text)
-    code = r.status_code
-    if code == 400:
-        data['error'] = {'code':'badreque','text':'권한이 없습니다.'} # 아마 이 것도 출력 될 일이 없을 것이다.
-        data['list'] = []
-    else:
-        data['list'] = rdat
-    return response(data)
-
-
 
 @app.route('/sqc/encode', methods=['POST'])
 def sqcEncode():
